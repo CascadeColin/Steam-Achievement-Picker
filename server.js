78827c0e066e4e04ff5e4285b89fa3be9907354c
Faruk Ajiya <Ajiyafaruk@gmail.com>
2023-01-13 03:35:08 -0800

    added dependencies

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
 const app = express();
 // sets PORT for Heroku, or 3001 if developing on localhost
-const PORT = process.env.PORT || 3002;
-const routes = require('./routes');
-const path = require('path');
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: process.env.Secret,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // built-in JSON parser
 app.use(express.json());
@@ -16,4 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // tells express to use imported routers
 app.use(routes);
 
+sequelize.sync({ force: false }).then(() => {
 app.listen(PORT, () => console.log(`Hosted on port ${PORT}`));
+});
