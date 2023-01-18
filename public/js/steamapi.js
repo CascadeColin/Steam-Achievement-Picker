ecf85fb4258e07d686588ac03c6cad6c0174ca9c
Faruk Ajiya <Ajiyafaruk@gmail.com>
2023-01-17 20:14:58 -0800

    steam api fetch

--- public/js/steamapi.js
+++ public/js/steamapi.js
@@ -21,6 +21,7 @@ function UserData(){
     })
 }
 
+//gets the appid for each game
 function UsersGameData(){
     var queryURL = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="+apikey+"&steamid="+steamid+"&format=jsonArguments";
     fetch(queryURL)
@@ -31,13 +32,14 @@ function UsersGameData(){
         //show the current number of games the user has
          gameCount = data.response.game_count;
          //shows the appid for each game
+         const gamesAppId =Array(gameCount).fill(null)
         for(var i = 0; i <  gameCount; i++){
-            //TODO:create an array that will take all the appid for each game 
+            gamesAppId[i] = data.response.games[i].appid;
         }
     })
 }
 
-//gets the name of the game and achievemnts for each appid
+//gets the name of the game and achievemnts for each game both completed and uncompleted
 function GameData(){
     var queryURL = "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid="+appid+"key="+apikey+"&steamid="+steamid;
     fetch(queryURL)
@@ -46,20 +48,50 @@ function GameData(){
     })
     .then(function (data) {
         GameName = data.playerstats.gameName;
-        //TODO:create an array that will get the name for each game
-        //TODO:create an array that will get achievements that have been completed
-        //TODO:create an array that will get achievements that have not been completed
+        achievementCount = data.playerstats.achievements.lenght;
+        const achievementscompletedname =[];
+        const achievementsuncompletedname = [];
+        for(var i = 0; i <  achievementCount; i++){
+            if (data.playerstats.achievements[i].achieved === 1) {
+                achievementCompleted = achievementCompleted + 1;
+                achievementscompletedname[i] = data.playerstats.achievements[i].apiname;
+            } else {
+                achievementsUncompleted = achievementsUncompleted + 1;
+                achievementsuncompletedname[i] = data.playerstats.achievements[i].apiname
+            }
+        }
     })
 }
 
-//gets the users friends list
-function FriendsData(){
+//gets the users friend's name
+function FriendsSteamID(){
     var queryURL = " http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key="+apikey+"&steamid="+steamid+"&relationship=friend";
     fetch(queryURL)
     .then(function(response){
        return response.json();
     })
     .then(function (data) {
-        //TODO:create an array that will store a friends steam id and use the steam id to make a fetch request from the api to be able to get the firends name or username
+        friendsCount = data.friendslist.friends.lenght;
+        const friendsSteamId = [];
+        for(var i = 0; i <  friendsCount; i++){
+            friendsSteamId[i] = data.friendslist.friends[i].steamid;
+        }
+        return friendsSteamId;
+    })
+    .then(function (friendsSteamId) {
+        const friendsname = [];
+        friendsSteamId.forEach( id => {
+            var queryURL = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="+apikey+"9&steamids="+id;
+            fetch(queryURL)
+            .then(function(response){
+               return response.json();
+            })
+            .then(function (data) {
+                friendsname.push(data.response.players.personaname)
+            })
+        });
+
     })
 }
+
+//TODO:create a function that will fetch the logo for each game 
