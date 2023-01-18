1e9ea636cd3ede67ba8051333e5be0ca93965833
Colin Marshall <colin.marshall94@gmail.com>
2023-01-17 21:08:19 -0800

    updated model associations

--- models/index.js
+++ models/index.js
@@ -3,39 +3,49 @@ const ownedGames = require("./OwnedGames");
 const feedback = require("./feedback");
 const Achievements = require("./Achievement");
 
+// user has many ownedGames (1:N)
+// ownedGames has many achievements (1:N)
+// achievements has many feedback (1:N)
+// user has many feedback (1:N)
+
 User.hasMany(ownedGames, { 
-    foreignKey: 'userId',
-    onDelete: "CASCADE", 
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE', 
 });
 
 ownedGames.belongsTo(User, { 
-    foreignKey: 'userId' 
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL' 
 });
 
 ownedGames.hasMany(Achievements, {
-    foreignKey: "game_id"
+    foreignKey: 'game_id',
+    onDelete: 'CASCADE',
 });
+
 Achievements.belongsTo(ownedGames, {
-    foreignKey: "game_id"
+    foreignKey: 'game_id',
+    onDelete: 'SET NULL'
 });
 
 User.hasMany(feedback, {
-    foreignKey: "user_id",
-    onDelete: "CASCADE",
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
 });
 
 feedback.belongsTo(User, {
     foreignKey: "user_id",
+    onDelete: 'SET NULL'
 });
 
 Achievements.hasMany(feedback, {
-    foreignKey: "achievement_id",
+    foreignKey: 'achievement_id',
+    onDelete: 'CASCADE',
 });
 
 feedback.belongsTo(Achievements, {
-    foreignKey: "achievement_id",
+    foreignKey: 'achievement_id',
+    onDelete: 'SET NULL'
 });
-
-
 
 module.exports = { User, ownedGames, feedback, Achievements };
