0ce1262fab9ebaf63d8d93a61d8789f0664be5ce
Colin Marshall <colin.marshall94@gmail.com>
2023-01-17 18:51:17 -0800

    created helpers && pseudocode for handling login/logout state

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+/* This is where helper functions will go. */
+
+// TODO: import modules
+
+// TODO: will check if user is logged in or not and render the view accordingly
+const loginState = () => {
+    // if logged in, render all site features
+    // else, redirect to login page
+}
+
+module.exports = loginState;
