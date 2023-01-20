1788d6cf180b02cc77302f3f298b4c457d847222
Colin Marshall <colin.marshall94@gmail.com>
2023-01-20 12:41:37 -0800

    refactored models and updated comments

--- models/achievement.js
+++ models/achievement.js
@@ -6,44 +6,43 @@ const sequelize = require("../config/connection");
 class achievement extends Model {}
 
 achievement.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      // will be called as achievement.name, adding achievement_name was redundant
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      achieved: {
-        // unsure if this will actually be handled as a boolean.  Steam API data returns 0 for unachieved and 1 for unachieved.  will research how sequelize will handle that but I know mysql interprets 0=false and 1=true - Colin
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-      },
-      // timestamp of when the achievement was earned
-      unlock_time: {
-        type: DataTypes.INTEGER,
-        // Steam API returns 
-        allowNull: false,
-      },
-      game_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: "ownedGame",
-          key: "appid",
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    achieved: {
+      // unsure if this will actually be handled as a boolean.  Steam API data returns 0 for unachieved and 1 for unachieved.  will research how sequelize will handle that but I know mysql interprets 0=false and 1=true - Colin
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    // timestamp of when the achievement was earned
+    unlock_time: {
+      type: DataTypes.INTEGER,
+      // Steam API returns
+      allowNull: false,
+    },
+    game_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "ownedGame",
+        key: "appid",
       },
     },
-    {
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: "achievement",
-    }
-  );
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "achievement",
+  }
+);
 
 module.exports = achievement;
