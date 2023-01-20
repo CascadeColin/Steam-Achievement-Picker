5fb5679fff9ade0160933c533216fe9a76aac1de
Colin Marshall <colin.marshall94@gmail.com>
2023-01-20 13:39:54 -0800

    fixed bugs in signup, it now creates a user

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,13 +2,17 @@ const signupFormHandler = async (event) => {
     event.preventDefault();
   
     const email = document.querySelector("#email-signup").value.trim();
-    const steamid = document.querySelector("#steamid-signup").value.trim();
+    // HTML form inputs return strings, but we need a number
+    const steamidNum = document.querySelector("#steamid-signup").value.trim();
+    const steamid = parseInt(steamidNum);
     const password = document.querySelector("#password-signup").value.trim();
+
+    console.log(typeof email, email, typeof password, password, typeof steamid, steamid)
   
     if (email && steamid && password) {
       const response = await fetch("/api/users/signup", {
         method: "POST",
-        body: JSON.stringify({ email, steamid, password }),
+        body: JSON.stringify({ email, password, steamid }),
         headers: { "Content-Type": "application/json" },
       });
   
