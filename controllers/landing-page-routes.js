811577b2f770455e0f42c9f11898de9da3631170
Colin Marshall <colin.marshall94@gmail.com>
2023-01-23 01:05:11 -0800

    added link to dashboard on homepage navbar, and both now require users to be logged in to view

--- controllers/landing-page-routes.js
+++ controllers/landing-page-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { feedback, user, ownedGame, achievement } = require('../models');
 const sequelize = require('.././config/connection');
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
     res.render('landing-page', {
@@ -16,11 +17,13 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.get('/dashboard', (req,res) => {
+router.get('/dashboard', withAuth, (req,res) => {
     res.render('dashboard');
 })
 
-
+router.get('/achievements', withAuth, (req,res) => {
+    res.render('my-achievements');
+})
 
 router.get('/signup', (req, res) => {
     res.render('signup');
