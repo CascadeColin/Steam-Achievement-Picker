8941d2406e3d2025b47ade765a33267905a3e549
Colin Marshall <colin.marshall94@gmail.com>
2023-01-23 00:59:05 -0800

    fixed navbar on landing-page to recognize logged in state

--- controllers/landing-page-routes.js
+++ controllers/landing-page-routes.js
@@ -3,16 +3,24 @@ const { feedback, user, ownedGame, achievement } = require('../models');
 const sequelize = require('.././config/connection');
 
 router.get('/', (req, res) => {
-    res.render('landing-page');
+    res.render('landing-page', {
+        loggedIn: req.session.loggedIn
+    });
 })
 
 router.get('/login', (req, res) => {
     if(req.session.loggedIn) {
-        res.redirect('/');
+        res.redirect('/dashboard');
         return; 
     }
     res.render('login');
 });
+
+router.get('/dashboard', (req,res) => {
+    res.render('dashboard');
+})
+
+
 
 router.get('/signup', (req, res) => {
     res.render('signup');
