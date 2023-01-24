0b2b76518963fa7332c40b00c06e0540989f1197
Colin Marshall <colin.marshall94@gmail.com>
2023-01-23 18:05:47 -0800

    updated files

--- controllers/landing-page-routes.js
+++ controllers/landing-page-routes.js
@@ -1,36 +1,47 @@
-const router = require('express').Router();
-const { feedback, user, ownedGame, achievement } = require('../models');
-const sequelize = require('.././config/connection');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { feedback, user, ownedGame, achievement } = require("../models");
+const sequelize = require(".././config/connection");
+const withAuth = require("../utils/auth");
 
-router.get('/', (req, res) => {
-    res.render('landing-page', {
-        loggedIn: req.session.loggedIn
-    });
-})
-
-router.get('/login', (req, res) => {
-    if(req.session.loggedIn) {
-        res.redirect('/dashboard');
-        return; 
-    }
-    res.render('login');
+router.get("/", (req, res) => {
+  res.render("landing-page", {
+    loggedIn: req.session.loggedIn,
+  });
 });
 
-router.get('/dashboard', withAuth, (req,res) => {
-    res.render('dashboard', {
-        loggedIn: req.session.loggedIn
-    });
-})
-
-router.get('/achievements', withAuth, (req,res) => {
-    res.render('my-achievements', {
-        loggedIn: req.session.loggedIn
-    });
-})
-
-router.get('/signup', (req, res) => {
-    res.render('signup');
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard");
+    return;
+  }
+  res.render("login");
 });
 
-module.exports = router; 
+router.get("/dashboard", withAuth, (req, res) => {
+  res.render("dashboard", {
+    loggedIn: req.session.loggedIn,
+  });
+});
+
+router.get("/achievements", withAuth, (req, res) => {
+  res.render("my-achievements", {
+    loggedIn: req.session.loggedIn,
+  });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+// if any other route typed in URL, render homepage
+router.get("*", (req, res) => {
+  if (req.session.loggedIn) {
+    res.render("dashboard", {
+      loggedIn: req.session.loggedIn,
+    });
+  } else {
+    res.render("landing-page");
+  }
+});
+
+module.exports = router;
