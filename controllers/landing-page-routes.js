bf01b19119f57825cef33409e99f2d1f49e77e5e
Colin Marshall <colin.marshall94@gmail.com>
2023-01-24 05:21:50 -0800

    dashboard randomly chooses 4 games and will display a random achievement if selected, also randomized the picker

--- controllers/landing-page-routes.js
+++ controllers/landing-page-routes.js
@@ -3,6 +3,7 @@ const { feedback, user, ownedGame, achievement } = require("../models");
 const sequelize = require(".././config/connection");
 const withAuth = require("../utils/auth");
 const {Op} = require('sequelize');
+const shuffle = require('../utils/shuffle');
 
 router.get("/", async(req, res) => {
   res.render("landing-page", {
@@ -17,8 +18,37 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.get("/dashboard", withAuth, (req, res) => {
+router.get("/dashboard", withAuth, async (req, res) => {
+  const steam_id = req.session.steamid;
+  // find user in db by that steamid
+  const currentUser = await user.findOne({
+    where: {
+      steam_id: steam_id,
+    },
+  });
+
+  const pickGame = await ownedGame.findAll({
+    where: {
+      user_id: currentUser.dataValues.id
+    }
+  });
+
+  const randomGame = shuffle(pickGame);
+
+  const randomGames = {
+    game1: randomGame[0].dataValues.name,
+    game2: randomGame[1].dataValues.name,
+    game3: randomGame[2].dataValues.name,
+    game4: randomGame[3].dataValues.name,
+    game1url: `http://media.steampowered.com/steamcommunity/public/images/apps/${randomGame[0].dataValues.appid}/${randomGame[0].dataValues.img_icon_url}.jpg`,
+    game2url: `http://media.steampowered.com/steamcommunity/public/images/apps/${randomGame[1].dataValues.appid}/${randomGame[1].dataValues.img_icon_url}.jpg`,
+    game3url: `http://media.steampowered.com/steamcommunity/public/images/apps/${randomGame[2].dataValues.appid}/${randomGame[2].dataValues.img_icon_url}.jpg`,
+    game4url: `http://media.steampowered.com/steamcommunity/public/images/apps/${randomGame[3].dataValues.appid}/${randomGame[3].dataValues.img_icon_url}.jpg`,
+    game1appid: randomGame[0].dataValues.appid
+  }
+
   res.render("dashboard", {
+    randomGames,
     loggedIn: req.session.loggedIn,
   });
 });
@@ -50,15 +80,70 @@ router.get("/achievements", withAuth, async (req, res) => {
     return a = a.dataValues;
   })
   
-  console.dir(renderAchievements)
   res.render("all-achievements", {
     renderAchievements,
     loggedIn: req.session.loggedIn,
   });
 });
 
-router.get("/achievement-picker", withAuth, (req, res) => {
+router.get('/achievement-picker/:appid', withAuth, async (req,res) => {
+  try {
+    const game = await ownedGame.findOne({
+      where: {
+        appid: req.params.appid
+      }
+    });
+    const searchAchievements = await achievement.findAll({
+      where: {
+        game_id: game.dataValues.id
+      }
+    });
+    const aNameArr = shuffle(searchAchievements.map(a => a.dataValues.name));
+
+    const randomAchievement = {
+      gName: game.dataValues.name,
+      aName: aNameArr[0]
+    }
+    console.log(randomAchievement)
+    res.status(200).render("achievement-picker", {
+      randomAchievement,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+router.get("/random-achievement-picker", withAuth, async (req, res) => {
+  const steam_id = req.session.steamid;
+    // find user in db by that steamid
+  const currentUser = await user.findOne({
+    where: {
+      steam_id: steam_id,
+    },
+  });
+
+  const pickGame = await ownedGame.findAll({
+    where: {
+      user_id: currentUser.dataValues.id
+    }
+  });
+  
+  const randomGame = shuffle(pickGame);
+  const pickAchievement = await achievement.findAll({
+    where: {
+      game_id: randomGame[0].dataValues.id
+    }
+  });
+  const randomAch = shuffle(pickAchievement);
+
+  const randomAchievement = {
+    aName: randomAch[0].dataValues.name,
+    gName: randomGame[0].dataValues.name
+  }
+  
   res.render("achievement-picker", {
+    randomAchievement,
     loggedIn: req.session.loggedIn,
   });
 });
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,10 @@
+function randomAchievementPicker() {
+    document.location.replace("/random-achievement-picker");
+}
 function achievementPicker() {
-    document.location.replace("/achievement-picker");
+    const appidselector = document.querySelector('#a')
+    console.log(appidselector.dataset.appid)
+    document.location.replace(`/achievement-picker/${appidselector.dataset.appid}`);
 }
 
 
@@ -7,26 +12,29 @@ document.querySelector(".card1").addEventListener("click", achievementPicker);
 document.querySelector(".card2").addEventListener("click", achievementPicker);
 document.querySelector(".card3").addEventListener("click", achievementPicker);
 document.querySelector(".card4").addEventListener("click", achievementPicker);
-document.querySelector(".generator-btn").addEventListener("click", achievementPicker);
+document.querySelector(".generator-btn").addEventListener("click", randomAchievementPicker);
 
 var game1EL = document.querySelector(".game1");
 var game2EL = document.querySelector(".game2");
 var game3EL = document.querySelector(".game3");
 var game4EL = document.querySelector(".game4");
 
+const games = [game1EL, game2EL, game3EL, game4EL];
 
-async function  getgamenameandimage() {
+async function getgamenameandimage() {
     try {
         const res = await fetch('/api/ownedgames');
         const data = await res.json();
-        game1name = data[0].name;
-        game2name = data[1].name;
-        game3name = data[2].name;
-        game4name = data[3].name;
-        game1EL.textContent=game1name;
-        game2EL.textContent=game2name;
-        game3EL.textContent=game3name;
-        game4EL.textContent=game4name;
+        const gameNames = data.map(obj => obj = obj.name);
+
+        // game1name = data[0].name;
+        // game2name = data[1].name;
+        // game3name = data[2].name;
+        // game4name = data[3].name;
+        // game1EL.textContent=game1name;
+        // game2EL.textContent=game2name;
+        // game3EL.textContent=game3name;
+        // game4EL.textContent=game4name;
     } catch (err) {
         console.log(err);
     }
