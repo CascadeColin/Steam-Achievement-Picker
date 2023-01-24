6d39c7de93b9090e4a6567105fb17f1bb213f270
Colin Marshall <colin.marshall94@gmail.com>
2023-01-23 23:22:21 -0800

    various bug fixes

--- public/js/add-feedback.js
+++ public/js/add-feedback.js
@@ -1,8 +1,15 @@
+// wait until 1ms after animation
+const timeout = setTimeout(() => {
+  const btn = document
+    .querySelector(".achievement-name-container")
+    .addEventListener("click", addFeedbackHandler);
+}, 2501);
+
 const addFeedbackHandler = async (event) => {
   event.preventDefault();
   document.location.replace("/single-achievement");
 };
 
-document
-  .querySelector(".achievement-name-container")
-  .addEventListener("submit", addFeedbackHandler);
+// document
+//   .querySelector(".achievement-name-container")
+//   .addEventListener("click", addFeedbackHandler);
