2f1dd7fbd7180cbf99354efad9db658875b546d4
Colin Marshall <colin.marshall94@gmail.com>
2023-01-24 16:57:10 -0800

    MVP :)

--- public/js/add-feedback.js
+++ public/js/add-feedback.js
@@ -1,13 +1,12 @@
 // wait until 1ms after animation
 const timeout = setTimeout(() => {
-  const btn = document
-    .querySelector(".achievement-name-container")
-    .addEventListener("click", addFeedbackHandler);
+  const btn = document.querySelector(".achievement-name-container");
+  addEventListener("click", addFeedbackHandler);
 }, 2501);
 
-const addFeedbackHandler = async (event) => {
+const addFeedbackHandler = (event) => {
   event.preventDefault();
-  document.location.replace("/single-achievement");
+  document.location.replace(`/single-achievement/${event.target.dataset.id}`);
 };
 
 // document
